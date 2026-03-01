5c1fc79be9e25153183e45af73547892d68f4311
bnpallavi <150516198+bnpallavi@users.noreply.github.com>
2026-03-01 23:42:44 +0530

    Update qspi_driver.sv

--- uvm/qspi_driver.sv
+++ uvm/qspi_driver.sv
@@ -8,28 +8,44 @@ class qspi_driver extends uvm_driver #(qspi_item);
       super.new(name,parent);
    endfunction
 
+
    function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+
       if(!uvm_config_db#(virtual qspi_if)::get(this,"","vif",vif))
          `uvm_fatal("NOVIF","Interface not set")
    endfunction
 
+
    task run_phase(uvm_phase phase);
-      qspi_item tr;
 
-      forever begin
-         seq_item_port.get_next_item(tr);
+   qspi_item tr;
 
-         vif.start      <= 1;
-         vif.opcode     <= tr.opcode;
-         vif.address    <= tr.address;
-         vif.write_data <= tr.write_data;
-         vif.burst_len  <= tr.burst_len;
+   // Wait for reset release
+   @(posedge vif.reset_n);
+   @(posedge vif.clk);
 
-         @(posedge vif.clk);
-         vif.start <= 0;
+   forever begin
 
-         seq_item_port.item_done();
-      end
-   endtask
+      seq_item_port.get_next_item(tr);
+
+      vif.start      <= 1;
+      vif.opcode     <= tr.opcode;
+      vif.address    <= tr.address;
+      vif.write_data <= tr.write_data;
+      vif.burst_len  <= tr.burst_len;
+
+      @(posedge vif.clk);
+      vif.start <= 0;
+
+      // Wait until CS toggles LOW then HIGH
+      @(negedge vif.CS);
+      @(posedge vif.CS);
+
+      seq_item_port.item_done();
+   end
+
+endtask
 
 endclass
+
