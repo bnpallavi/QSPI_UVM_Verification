c59e090b7525810250f44c439f316146b3c23660
bnpallavi <150516198+bnpallavi@users.noreply.github.com>
2026-03-26 14:54:30 +0530

    Update qspi_pkg.sv

--- uvm/qspi_pkg.sv
+++ uvm/qspi_pkg.sv
@@ -1,16 +1,52 @@
 package qspi_pkg;
 
-   import uvm_pkg::*;
-   `include "uvm_macros.svh"
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
 
-   `include "qspi_item.sv"
-   `include "qspi_driver.sv"
-   `include "qspi_monitor.sv"
-   `include "qspi_scoreboard.sv"   // NEW
-   `include "qspi_sequencer.sv"
-   `include "qspi_agent.sv"
-   `include "qspi_sequence.sv"
-   `include "qspi_test.sv"
+  //-------------------------
+  // TRANSACTION
+  //-------------------------
+  `include "qspi_item.sv"
+
+  //-------------------------
+  // COMPONENTS
+  //-------------------------
+  `include "qspi_driver.sv"
+  `include "qspi_monitor.sv"
+  `include "qspi_scoreboard.sv"
+  `include "qspi_sequencer.sv"
+  `include "qspi_agent.sv"
+
+  //-------------------------
+ // SEQUENCES
+`include "qspi_sequence.sv"
+`include "qspi_quad_sequence.sv"
+`include "qspi_seq_smoke.sv"
+`include "qspi_seq_boundary.sv"
+`include "qspi_seq_opcode.sv"
+`include "qspi_seq_burst.sv"
+`include "qspi_seq_random.sv"
+`include "qspi_seq_reset.sv"
+`include "qspi_seq_opcode_full.sv"
+
+
+
+// TESTS (ALWAYS LAST)
+`include "qspi_test.sv"
+`include "qspi_test_smoke.sv"
+`include "qspi_test_boundary.sv"
+`include "qspi_test_opcode.sv"
+`include "qspi_test_burst.sv"
+`include "qspi_test_random.sv"
+`include "qspi_test_reset.sv"
+`include "qspi_test_opcode_full.sv"
+
+
+
+
+
+  
 
 endpackage
+
 
