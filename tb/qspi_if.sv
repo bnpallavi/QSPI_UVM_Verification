217ed7c0fcdbe56e9503581ed43469299db27862
bnpallavi <150516198+bnpallavi@users.noreply.github.com>
2026-04-09 19:58:05 +0530

    Update qspi_if.sv

--- tb/qspi_if.sv
+++ tb/qspi_if.sv
@@ -2,25 +2,62 @@
 
 interface qspi_if(input logic clk);
 
+   //-----------------------------------
+   // CONTROL SIGNALS
+   //-----------------------------------
+
    logic reset_n;
    logic start;
+
+   //-----------------------------------
+   // INPUT DATA
+   //-----------------------------------
 
    logic [7:0]  opcode;
    logic [23:0] address;
    logic [7:0]  write_data;
    logic [3:0]  burst_len;
 
+   //-----------------------------------
+   // SPI OUTPUT
+   //-----------------------------------
+
    logic CS;
    logic SCLK;
-   tri   [3:0] IO;
+
+   tri [3:0] IO;
+
+   //-----------------------------------
+   // DRIVER CLOCKING BLOCK
+   //-----------------------------------
 
    clocking drv_cb @(posedge clk);
-      output start, opcode, address, write_data, burst_len;
+
+      output start;
+      output opcode;
+      output address;
+      output write_data;
+      output burst_len;
+
    endclocking
 
+
+   //-----------------------------------
+   // MONITOR CLOCKING BLOCK
+   //-----------------------------------
+
    clocking mon_cb @(posedge clk);
-      input start, opcode, address, write_data, CS, SCLK, IO;
+
+      input start;
+      input opcode;
+      input address;
+      input write_data;
+      input burst_len;
+
+      input CS;
+      input SCLK;
+      input IO;
+
    endclocking
 
 endinterface
-
